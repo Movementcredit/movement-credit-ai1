dfdeaa5c246a531b269538edb52f5f7ef3d94866
Movement Credit A.I <movementcredit251@gmail.com>
2025-07-17 21:24:52 -0500

    Update credit_dispute_generator.py

--- credit_dispute_generator.py
+++ credit_dispute_generator.py
@@ -1,4 +1,3 @@
-# Simple version without PDF generation for deployment
 import os
 from datetime import datetime
 
@@ -9,10 +8,8 @@ class EnhancedCreditDisputeGenerator:
         self.output_files = []
 
     def generate_3bureau_dispute_letters_from_report(self, report_text):
-        """Generate dispute letters as text files instead of PDFs"""
         generated_files = []
         
-        # Sample derogatory items to demonstrate functionality
         sample_items = [
             "Unverified late payment on Capital One account",
             "Incorrect balance on Chase credit card", 
@@ -22,13 +19,11 @@ class EnhancedCreditDisputeGenerator:
         bureaus = ['Equifax', 'Experian', 'TransUnion']
         
         for bureau in bureaus:
-            filename = f"{bureau}_dispute_letter.txt"
+            filename = bureau + "_dispute_letter.txt"
             filepath = os.path.join(os.getcwd(), filename)
             
-            # Generate letter content
             letter_content = self._generate_letter_content(bureau, sample_items)
             
-            # Write to text file
             with open(filepath, 'w', encoding='utf-8') as f:
                 f.write(letter_content)
             
@@ -37,44 +32,33 @@ class EnhancedCreditDisputeGenerator:
         return generated_files
     
     def _generate_letter_content(self, bureau, items):
-        """Generate the dispute letter content"""
         user_name = self.user_data.get('full_name', 'Your Name')
-        user_address = f"{self.user_data.get('street_address', '')}, {self.user_data.get('city', '')}, {self.user_data.get('state', '')} {self.user_data.get('zip_code', '')}"
+        street = self.user_data.get('street_address', '')
+        city = self.user_data.get('city', '')
+        state = self.user_data.get('state', '')
+        zip_code = self.user_data.get('zip_code', '')
+        user_address = street + ", " + city + ", " + state + " " + zip_code
         
-        letter = f"""
-{self.current_date}
-
-{bureau} Credit Bureau
-Consumer Relations Department
-Atlanta, GA 30374
-
-Re: Credit Report Dispute - {user_name}
-Last 4 SSN: {self.user_data.get('ssn_last4', 'XXXX')}
-Date of Birth: {self.user_data.get('dob', 'XX/XX/XXXX')}
-
-Dear {bureau} Dispute Department,
-
-I am writing to formally dispute the following items on my credit report:
-
-"""
+        letter = self.current_date + "\n\n"
+        letter += bureau + " Credit Bureau\n"
+        letter += "Consumer Relations Department\n"
+        letter += "Atlanta, GA 30374\n\n"
+        letter += "Re: Credit Report Dispute - " + user_name + "\n"
+        letter += "Last 4 SSN: " + self.user_data.get('ssn_last4', 'XXXX') + "\n"
+        letter += "Date of Birth: " + self.user_data.get('dob', 'XX/XX/XXXX') + "\n\n"
+        letter += "Dear " + bureau + " Dispute Department,\n\n"
+        letter += "I am writing to formally dispute the following items on my credit report:\n\n"
         
         for i, item in enumerate(items, 1):
-            letter += f"{i}. {item}\n"
+            letter += str(i) + ". " + item + "\n"
         
-        letter += f"""
-
-I am requesting that these items be investigated and removed from my credit report as they are inaccurate, unverifiable, or outdated. Under the Fair Credit Reporting Act, you have 30 days to investigate these disputes.
-
-Please provide me with written documentation of your investigation results and any changes made to my credit report.
-
-Thank you for your prompt attention to this matter.
-
-Sincerely,
-
-{user_name}
-{user_address}
-Phone: {self.user_data.get('phone', '')}
-Email: {self.user_data.get('email', '')}
-"""
+        letter += "\nI am requesting that these items be investigated and removed from my credit report as they are inaccurate, unverifiable, or outdated. Under the Fair Credit Reporting Act, you have 30 days to investigate these disputes.\n\n"
+        letter += "Please provide me with written documentation of your investigation results and any changes made to my credit report.\n\n"
+        letter += "Thank you for your prompt attention to this matter.\n\n"
+        letter += "Sincerely,\n\n"
+        letter += user_name + "\n"
+        letter += user_address + "\n"
+        letter += "Phone: " + self.user_data.get('phone', '') + "\n"
+        letter += "Email: " + self.user_data.get('email', '') + "\n"
         
         return letter
